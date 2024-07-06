a4f2a86f0542e64bb5c96a1c7c72a2b6397fff6d
Abdul Rehman <talhaabbas79@gmail.com>
2024-07-06 10:59:31 +0500

    drag drop functionality added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".main");
 const addListBtn = document.querySelector(".add-another-list");
 
+let dropTask = null;
+
 const createTask = (event) => {
   event.preventDefault();
 
@@ -33,12 +35,12 @@ const createList = (listTitle) => {
   const cardForm = document.createElement("form");
   const cardInput = document.createElement("input");
 
-  const h3List = document.createTextNode(listTitle)
+  const h3List = document.createTextNode(listTitle);
 
   listDiv.classList.add("list");
   listH3.classList.add("list-title");
-  cardForm.classList.add("card-form")
-  cardInput.classList.add("card-form-input")
+  cardForm.classList.add("card-form");
+  cardInput.classList.add("card-form-input");
 
   cardInput.setAttribute("type", "text");
   cardInput.setAttribute("placeholder", "+ Add a card");
@@ -51,21 +53,40 @@ const createList = (listTitle) => {
 
   cardForm.addEventListener("submit", createTask);
 
+  listDiv.addEventListener("dragleave", (event) => event.preventDefault());
+  listDiv.addEventListener("dragover", (event) => event.preventDefault());
+
+  listDiv.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const dropTarget = event.target;
+
+    if (dropTarget.classList.contains("list")) {
+      dropTarget.appendChild(dropTask);
+    }
+    else if (dropTarget.classList.contains("card-task")) {
+      dropTarget.parentElement.appendChild(dropTask);
+    }
+
+  });
   return listDiv;
-};
+}
 
 const addTask = (value) => {
   const cardPara = document.createElement("p");
   const cardText = document.createTextNode(value);
 
-  cardPara.classList.add("card-task")
+  cardPara.classList.add("card-task");
   cardPara.setAttribute("draggable", "true");
   cardPara.appendChild(cardText);
+
+  cardPara.addEventListener("mousedown", (event) => {
+    dropTask = event.target;
+  });
 
   return cardPara;
 };
 
-let savedTasks = JSON.parse(localStorage.getItem("savedTasks")); 
+let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
 
 if (!savedTasks) {
   savedTasks = {};
@@ -87,6 +108,8 @@ for (const title in savedTasks) {
 
 addListBtn.addEventListener("click", () => {
   const listTitle = prompt("Enter list title");
+
+  if (!listTitle) return;
 
   const taskCard = createList(listTitle);
 
